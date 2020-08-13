50f12bad3e1204a673369ef9b7cee39fc40dc056
Marc-Antoine Ross <marc@proze.net>
2020-08-13 00:18:06 -0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Button from "@material-ui/core/Button"
+import Container from "@material-ui/core/Container"
+import Typography from "@material-ui/core/Typography"
+import Box from "@material-ui/core/Box"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import { ThemeProvider } from "@material-ui/core/styles"
+import { createMuiTheme } from "@material-ui/core/styles"
+// import ProTip from "./ProTip"
 
-function App() {
+export const App: React.FC = () => {
+  // TODO: add toggle for dark mode
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#25c2a0",
+        contrastText: "#fff",
+      },
+      secondary: {
+        main: "#86b8b6",
+        contrastText: "#fff",
+      },
+      success: {
+        main: "#25c2a0",
+        contrastText: "#fff",
+      },
+      info: {
+        main: "#43b5c5",
+        contrastText: "#fff",
+      },
+      error: {
+        main: "#fa62fc",
+        contrastText: "#fff",
+      },
+    },
+  })
+
+  function login() {
+    // @ts-ignore
+    window.solana
+      .request({
+        method: "wallet_requestAccounts",
+      })
+      .then((resp: any) => {
+        if (resp.err) {
+          console.log("error", resp.err)
+          return
+        }
+
+        console.log("got accounts:", resp.result.accounts)
+      })
+      .catch((err: any) => {
+        console.log("promised returned err: ", err)
+      })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
 
-export default App;
+      <Container maxWidth="sm">
+        <Box my={4} display="flex" flexDirection="column" alignItems="center">
+          <Typography color="primary" variant="h4" component="h1" align="center" gutterBottom>
+            Welcome to MegaDapp
+          </Typography>
+          <Button variant="contained" color="primary" onClick={login}>
+            Login
+          </Button>
+        </Box>
+      </Container>
+    </ThemeProvider>
+  )
+}
